d4f8d70f71146982357890d8c54adab52120ed11
Daniel Demidko <demidko@farpost.com>
2020-11-11 22:19:47 +1000

    Переходим на MS rest sdk

--- Main.cpp
+++ Main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
-#include <restinio/all.hpp>
 
 int main() {
 
-  restinio::run(
-    restinio::on_thread_pool(4)
-      .port(80)
-      .address("localhost")
-      .request_handler([](auto req) {
-        return req->create_response().set_body("Hello, World!").done();
-      }));
 }
