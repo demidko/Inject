222a7fd39fdf0fa2d50eb6044bde5f7ba51d0d50
Daniel Demidko <demidko@farpost.com>
2020-11-11 19:10:42 +1000

    Инструкции

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <restinio/all.hpp>
 
-int main() {
+int main(int argc, char **args) {
+  if (argc != 1) throw std::runtime_error("Usage: ");
+  auto port = std::stoi(args[0]);
   restinio::run(
-      restinio::on_thread_pool(4)
-          .port(80)
-          .address("localhost")
-          .request_handler([](auto req) {
-            return req->create_response().set_body("Hello, World!").done();
-          }));
+    restinio::on_thread_pool(4)
+      .port(port)
+      .address("localhost")
+      .request_handler([](auto req) {
+        return req->create_response().set_body("Hello, World!").done();
+      }));
 }
