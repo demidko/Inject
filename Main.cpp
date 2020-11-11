817356486eb92481f6f78e59f7a6caf6386d2b8c
Daniel Demidko <demidko@farpost.com>
2020-11-11 20:22:19 +1000

    Добавляю подсветку типов

--- Main.cpp
+++ Main.cpp
@@ -2,11 +2,9 @@
 #include <restinio/all.hpp>
 
 int main(int argc, char **args) {
-  if (argc != 1) throw std::runtime_error("Usage: Inject [port]\nExample: Inject 80");
-  auto port = std::stoi(args[0]);
   restinio::run(
     restinio::on_thread_pool(4)
-      .port(port)
+      .port(80)
       .address("localhost")
       .request_handler([](auto req) {
         return req->create_response().set_body("Hello, World!").done();
